bfb41377a24f0f3d140113f8b30ce5ef3024f3ec
Luca1995it <luca.diliello@studenti.untin.it>
2019-05-13 18:13:46 +0200

    debugging

--- server/test.py
+++ server/test.py
@@ -1,6 +1,6 @@
 import OpenstackDriver
 
 d = OpenstackDriver.OpenstackDriver()
-clus = d._create_cluster("test", "", ["small-spark-node", "medium-spark-node"])
+clus = d._create_cluster("test", "", [])
 
-print(clus)
+print(clus.master_id, clus.network_id, clus.subnet_id, clus.router_id)
