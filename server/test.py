9ed476385d66a8995a98e2c34b6d8c56bf5da76b
Luca1995it <luca.diliello@studenti.untin.it>
2019-05-13 18:47:35 +0200

    fixed typo

--- server/test.py
+++ server/test.py
@@ -1,6 +1,6 @@
 import OpenstackDriver
 
 d = OpenstackDriver.OpenstackDriver()
-clus = d._create_cluster("test", "", [])
+clus = d._create_cluster("test", "")
 
 print(clus.master_id, clus.network_id, clus.subnet_id, clus.router_id)
