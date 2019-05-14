f93ba078bc84d155da82a23ee969b00e5e061353
Luca1995it <luca.diliello@studenti.untin.it>
2019-05-15 01:21:29 +0200

    clear.py comprehend database reset

--- server/clear.py
+++ server/clear.py
@@ -1,3 +1,4 @@
+from pymongo import MongoClient
 import OpenstackDriver
 
 d = OpenstackDriver.OpenstackDriver()
@@ -10,3 +11,7 @@ d._init_floating_ips()
 
 print("Deleting networks")
 d._init_networks()
+
+print("Resetting database")
+client = MongoClient('localhost', 27017)
+client.drop_database('ascm')
